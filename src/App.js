fe5fb41c40b9c59d3e089e233f59edc2078b54d0
Vincent <vincent.frank@yh.nackademin.se>
2019-11-05 12:58:40 +0100

    updated build

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import Todos from './Todos';
+import AddTodo from './AddTodo';
+
+export default class App extends Component {
+  state ={
+    todos: [
+      {id:1, content: 'buy some milk'},
+      {id:2, content: 'play the guiter'}
+    ]
+  }
+  addTodo = (todo) =>{
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos
+    })
+  }
+
+  deleteTodo = (id) =>{
+    const todos = this.state.todos.filter(
+      todo =>{
+        return todo.id !== id
+      }
+    );
+    return this.setState({todos:todos})
+  }
+  
+  render() {
+    return (
+      <div className='todo-app container'>
+       <h1 className= 'center blue-text'>Todo's</h1>
+       <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      <AddTodo addTodo={this.addTodo} />
+      </div>
+    )
+  }
+}
+
+
+
